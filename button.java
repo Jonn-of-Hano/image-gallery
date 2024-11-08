260acef7fac9b4d6b8cc0af8a13b1857720b38bd
Jonn-of-Hano <jonnofmark@gmail.com>
2024-11-07 17:48:54 -0700

    help

---
+++ button.java
@@ -0,0 +1,23 @@
+bear.addEventListener("click",function() {
+    document.getElementById("mainImage").src="images/bear.webp";
+})
+
+elk.addEventListener('click', function () {
+    document.getElementById("mainImage").src="images/elk.webp";
+});
+
+moose.addEventListener('click', function () {
+    document.getElementById("mainImage").src="images/moose.webp";
+});
+
+sheep.addEventListener('click', function () {
+    document.getElementById("mainImage").src="images/sheep.webp";
+});
+
+wolf.addEventListener('click', function () {
+    document.getElementById("mainImage").src="images/wolf.webp";
+});
+
+button.addEventListener("click", function() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}); 
